72232a2f4749375b5b34496cebecdff483477fe6
LA-1002 <LeonAli4002@gmail.com>
2022-10-18 14:17:06 +0100

    Final Commit of the Day... I Promise

--- mp3_converter.py
+++ mp3_converter.py
@@ -1,8 +1,8 @@
 from moviepy.editor import *
 
-m4 = r'C:\Users\leonn\Videos\Youtube\kameko\Holiday - KSI (cover by kameko).mp4'
 
 def AudioConvert(m4):
+    m4 = (r'%s'%(m4));
     sp = m4.split('.mp4');
     m3 = (r'%s'%(sp[0] + '.mp3'))
     video = VideoFileClip(m4);
